db380ae4a007f6462b19b60743230485d8cda4a3
lam <lmarshall@sdl.com>
2022-06-10 12:22:37 +0100

    LiveContent switch between HTTP and HTTPs

---
+++ runLiveContentAsHTTPS.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+SET DIR=%~dp0%
+CD %DIR%
+ECHO %DIR%
+
+java -cp LiveContentUtils.jar com.sdl.livecontentutils.runtime.RunAsHttpOrHttps HTTPS
+
+if errorlevel 1 goto ERRORING
+echo.
+echo Configuration changed, starting LiveContent...
+echo.
+@REM autoplay.exe
+goto END
+
+:ERRORING
+echo.
+echo ##############################################
+echo An error occurred cannot start LiveContent ...
+echo ##############################################
+echo.
+:END
+PAUSE
